70b6126d9ff59b62320dfa07b5aba48488122001
Erika <johana.951031@gmail.com>
2025-12-22 13:02:58 -0500

    add click to enlarge the image and close it

--- scripts/index.js
+++ scripts/index.js
@@ -26,7 +26,7 @@ const initialCards = [
 //Modal y botones
 const editPopup = document.querySelector(`#edit-popup`);
 const editButton = document.querySelector(`.profile__edit-button`);
-const closeButton = document.querySelector(`.popup__close`);
+const closeButton = editPopup.querySelector(`.popup__close`);
 //Campos del perfil en la pagina
 const profileName = document.querySelector(`.profile__title`);
 const profileJob = document.querySelector(`.profile__description`);
@@ -34,6 +34,11 @@ const profileJob = document.querySelector(`.profile__description`);
 const nameInput = editPopup.querySelector(`.popup__input_type_name`);
 const jobInput = editPopup.querySelector(`.popup__input_type_description`);
 const formElement = editPopup.querySelector(`.popup__form`);
+// Modal y botones
+const imageModal = document.querySelector(`#image-popup`);
+const modalImage = imageModal.querySelector(`.popup__image`);
+const modalCaption = imageModal.querySelector(`.popup__caption`);
+const imageModalCloseButton = imageModal.querySelector(`.popup__close`);
 
 function openModal(modal) {
     modal.classList.add(`popup_is-opened`);    
@@ -67,18 +72,31 @@ closeButton.addEventListener("click", function() {
     closeModal(editPopup);
 });
 
+imageModalCloseButton.addEventListener(`click`, function() {
+    closeModal(imageModal);
+});
+
 formElement.addEventListener(`submit`, handleProfileFormSubmit);
 
 const cardTemplate = document.querySelector(`#card-template`);
 const cardsContainer = document.querySelector(`.cards__list`);
 
-function getCardElement(name = "Unnamed place", link = "./images/placeholder.jpg") {
+function getCardElement(cardData) {
     const cardElement = cardTemplate.content.cloneNode(true);
-    cardElement.querySelector(`.card__title`).textContent = name;
+    cardElement.querySelector(`.card__title`).textContent = cardData.name;
 
     const cardImage = cardElement.querySelector(`.card__image`);
-    cardImage.src = link;
-    cardImage.alt = name;
+    cardImage.src = cardData.link;
+    cardImage.alt = cardData.name;
+
+    //Detector click agrandar img
+    cardImage.addEventListener(`click`, function() {
+        modalCaption.textContent = cardData.name;
+        modalImage.src = cardData.link;
+        modalImage.alt = cardData.name;
+
+        openModal(imageModal);
+    });
 
     //seleccionar el boton "me gusta"
     const likeButton = cardElement.querySelector(`.card__like-button`);
@@ -96,13 +114,13 @@ function handleLikeButton(evt) {
     evt.target.classList.toggle(`card__like-button_is-active`);
 }
 
-function renderCard(name, link, container) {
-    const cardElement = getCardElement(name, link);
+function renderCard(cardData, container) {
+    const cardElement = getCardElement(cardData);
     container.prepend(cardElement);
 }
 
 initialCards.forEach((card) => {
-    renderCard(card.name, card.link, cardsContainer);
+    renderCard(card, cardsContainer);
 });
 
 const addButton = document.querySelector(`.profile__add-button`);
@@ -124,11 +142,13 @@ function handleCardFormSubmit(evt) {
     const cardNameInput = addCardPopup.querySelector(`.popup__input_type_card-name`);
     const cardLinkInput = addCardPopup.querySelector(`.popup__input_type_url`);
 
-    const cardName = cardNameInput.value;
-    const cardLink = cardLinkInput.value;
-
+    const newCard ={
+        name: cardNameInput.value,
+        link: cardLinkInput.value
+    };
+   
     //crear y agregar la nueva tarjeta de img
-    renderCard(cardName, cardLink, cardsContainer);
+    renderCard(newCard, cardsContainer);
     //cerrar la ventana emergente
     closeModal(addCardPopup);
     //limpiar el formulario
