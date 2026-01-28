387b270845d19160f025c92d49a7f5a930b6c50c
Erika <johana.951031@gmail.com>
2026-01-28 16:08:37 -0500

    add utils.js function

--- scripts/utils.js
+++ scripts/utils.js
@@ -1,5 +1,16 @@
-import { openModal, closeModal, clearFormFields, resertFormValidation } from './utils.js';
+//Funcion para abrir cualquier  modal
+export function openModal(modal) {
+    modal.classList.add('popup_is-opened');
+    document.addEventListener('keydown', closePopupOnEsc);
+}
 
+//Funcion para cerrar cualquier modal
+export function closeModal(modal) {
+    modal.classList.remove('popup_is-opened');
+    document.removeEventListener('keydown', closePopupOnEsc);
+}
+
+//Funcion para cerrar modal con la tecla Escape
 function closePopupOnEsc(evt) {
     if (evt.key === "Escape") {
         const openedPopup = document.querySelector('.popup_is-opened');
@@ -7,16 +18,6 @@ function closePopupOnEsc(evt) {
             closeModal(openedPopup);
         }
     }
-}
-
-export function openModal(modal) {
-    modal.classList.add('popup_is-opened');
-    document.addEventListener('keydown', closePopupOnEsc);
-}
-
-export function closeModal(modal) {
-    modal.classList.remove('popup_is-opened');
-    document.removeEventListener('keydown', closePopupOnEsc);
 }
 
 //Verificar que el clic sea especificamente en la superposicion
@@ -39,7 +40,7 @@ export function resetFormValidation(formElement, settings) {
     const inputList = Array.from(formElement.querySelectorAll(settings.inputSelector));
     const buttonElement = formElement.querySelector(settings.submitButtonSelector);
 
-    //Limpiar c/d input
+    //Limpiar cada input
     inputList.forEach((inputElement) => {
         hideInputError(formElement, inputElement);
     });
@@ -49,3 +50,11 @@ export function resetFormValidation(formElement, settings) {
     buttonElement.disabled = true;
 }
 
+function hideInputError(formElement, inputElement) {
+    const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+    inputElement.classList.remove('popup__input_type_error');
+    if (errorElement) {
+        errorElement.classList.remove('popup__error_visible');
+        errorElement.textContent = '';        
+    }
+}
